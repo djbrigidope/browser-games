e5110e67f49ac4b5c94d83c13290929a2327150d
bj bautista <emailme@djbrigidope.com>
2017-05-04 11:25:06 -0700

    another commit

--- public/public2/jsing.js
+++ public/public2/jsing.js
@@ -1,5 +1,5 @@
 
-var shwiftyLevelTho =[
+var shwiftyLevelTho = [
   " o                                ",
   "                oo                ",
   "xxxx          xxxxx        xxxxxxx",
@@ -10,3 +10,26 @@ var shwiftyLevelTho =[
   "       @      x!!!!!!!!!!!!!!!!!xx",
   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 ];
+
+
+function level(shwiftyLevelTho) {
+  this.width = shwiftyLevelTho[0].length;
+  this.height = shwiftyLevelTho.length;
+  this.grid =[];
+  this.actors = [];
+
+  for  ()var y = 0; v < this.height; y++) {
+    var line = plan[y], gridline = [];
+    for (var x = 0; x < this.width; x++) {
+      var ch = line[x], fieldType = null;
+      var Actor = actorChars[ch];
+      if(Actor)
+        this.actors.push(new Actor(new Vector(x,y), ch));
+      else if (ch == "x")
+        fieldType = "wall";
+      else if  else if (ch == "!")
+        fieldType = "lava";
+      gridLine.push(fieldType);  
+    }
+  }
+}
