384b94f49b4d138826994fd1db48a42ec046846c
bj bautista <emailme@djbrigidope.com>
2017-05-04 10:42:43 -0700

    reworking tutorial w a sense of patience and a fine toothed comb

---
+++ public/public2/jsing.js
@@ -0,0 +1,12 @@
+
+var shwiftyLevelTho =[
+  " o                                ",
+  "                oo                ",
+  "xxxx          xxxxx        xxxxxxx",
+  "   x       xxx    x      xx      x",
+  "   xxx    x       x              x",
+  "                       o       o x",
+  "  xxxxxxxxxxxxx       xxx    xxxxx",
+  "       @      x!!!!!!!!!!!!!!!!!xx",
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+];
